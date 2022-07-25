9fb4bfe3485bfe4a04c764546c3f5b0276f658d5
tduncan94 <83376555+tduncan94@users.noreply.github.com>
2022-07-25 14:46:36 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const io = require('socket.io');
+
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer);
+
+const sockets = require('./sockets');
+const PORT = 3000;
+
+httpServer.listen(PORT, ()=> {
+    console.log(`Listening on port: ${PORT}...`)
+});
+
+sockets.listen(socketServer);
+
+
+// require('socket.io')(server, {
+//     cors: {
+//       origin: 'http://localhost:3000',
+//       methods: ['GET', 'POST']
+//     }
+//   });
